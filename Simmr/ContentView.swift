1f719d1eae8eed29a1b168630cf5d0ad5e860f27
Ajay Mann <ajaymann7@gmail.com>
2026-07-12 16:45:38 +0530

    Initial Commit

---
+++ Simmr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simmr
+//
+//  Created by Ajay Mann on 12/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simmr/SimmrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimmrApp.swift
+//  Simmr
+//
+//  Created by Ajay Mann on 12/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SimmrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
